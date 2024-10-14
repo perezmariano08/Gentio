5d9e7ebd573551eddf639e4d9101e9d0a5915438
Mariano Perez <perezmariano.pm@gmail.com>
2024-10-14 20:35:10 -0300

    eliminacion nosotros

--- src/components/Nosotros/NosotrosStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { ContainerStyled, WrapperStyled } from "../mockups/Mockups";
-
-export const NosotrosContainer = styled(ContainerStyled)`
-`
-
-export const NosotrosWrapper = styled(WrapperStyled)`
-
-`
